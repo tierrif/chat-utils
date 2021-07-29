37f12995040133c03570fc6f379f00772ac7e04d
HotLava03 <tierriff@gmail.com>
2021-07-29 21:01:12 +0100

    Fix messages not copying.

--- src/main/java/io/github/hotlava03/chatutils/mixin/CopyToClipboardMixin.java
+++ src/main/java/io/github/hotlava03/chatutils/mixin/CopyToClipboardMixin.java
@@ -1,25 +1,16 @@
 package io.github.hotlava03.chatutils.mixin;
 
-import net.minecraft.advancement.Advancement;
-import net.minecraft.advancement.AdvancementDisplay;
-import net.minecraft.advancement.AdvancementFrame;
-import net.minecraft.advancement.AdvancementRewards;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.toast.AdvancementToast;
 import net.minecraft.client.toast.SystemToast;
-import net.minecraft.client.toast.Toast;
-import net.minecraft.client.toast.ToastManager;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.Item;
 import net.minecraft.text.LiteralText;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashMap;
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 
 @Mixin(ClientPlayerEntity.class)
 public class CopyToClipboardMixin {
@@ -27,12 +18,12 @@ public class CopyToClipboardMixin {
     private void onSendChatMessageSDM(String message, CallbackInfo info) {
         if (message.startsWith("/chatmacros")) info.cancel();
         else return;
-        /*Toolkit.getDefaultToolkit().getSystemClipboard()
+        Toolkit.getDefaultToolkit().getSystemClipboard()
                 .setContents(new StringSelection(
                                 message.replaceFirst("/chatmacros ", "")
                                         .replace("§", "&")),
                         null);
-        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(new LiteralText("\u00a79ChatUtils \u00a78\u00BB \u00a77Copied to clipboard."));*/
+        // MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(new LiteralText("\u00a79ChatUtils \u00a78\u00BB \u00a77Copied to clipboard."));
         SystemToast.show(MinecraftClient.getInstance().getToastManager(),
                 SystemToast.Type.WORLD_GEN_SETTINGS_TRANSFER,
                 new LiteralText("ChatUtils"),
