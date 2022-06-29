cde31b16491ea3bc66de7ac7002dc03f93b55d8f
magnus <magnus@iastate.edu>
2022-06-29 10:31:41 -0500

    Copies only the URL if there is a URL

--- src/main/java/io/github/hotlava03/chatutils/mixin/CopyToClipboardMixin.java
+++ src/main/java/io/github/hotlava03/chatutils/mixin/CopyToClipboardMixin.java
@@ -15,20 +15,41 @@ import java.awt.datatransfer.StringSelection;
 
 @Mixin(ClientPlayerEntity.class)
 public class CopyToClipboardMixin {
+
+    String last_chat_message = "";
+
     @Inject(method = "sendCommand(Lnet/minecraft/network/message/ChatMessageSigner;Ljava/lang/String;Lnet/minecraft/text/Text;)V",
             at = @At("HEAD"),
             cancellable = true)
     private void onSendCommand(ChatMessageSigner signer, String command, Text preview, CallbackInfo ci) {
         if (command.startsWith("chatmacros ")) {
             ci.cancel();
+
+            String inform_player = "Text copied.";
+
+            if (!command.equals(last_chat_message)) {
+                last_chat_message = command;
+                // Search in message for the first URL and copy only that
+                String[] words = command.split(" ");
+                for (String word : words) {
+                    if (word.startsWith("https://") || word.startsWith("http://")) {
+                        last_chat_message = command;
+                        command = word;
+                        inform_player = "URL copied. Click again to copy entire message";
+                        break;
+                    }
+                }
+            }
+
             Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
                     new StringSelection(command.replaceFirst("chatmacros ", "").replace("§", "&")),
-                    null);
+                    null
+            );
             // MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(new LiteralText("\u00a79ChatUtils \u00a78\u00BB \u00a77Copied to clipboard."));
             SystemToast.show(MinecraftClient.getInstance().getToastManager(),
                     SystemToast.Type.WORLD_GEN_SETTINGS_TRANSFER,
                     Text.literal("ChatUtils"),
-                    Text.literal("Text copied.")
+                    Text.literal(inform_player)
             );
         }
     }
