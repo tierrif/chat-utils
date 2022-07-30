cf1af53a845880a44454a74fe23cc491897bd416
tierrif <tierriff@gmail.com>
2022-07-30 19:54:59 +0100

    Remove unused imports.

--- src/main/java/io/github/hotlava03/chatutils/mixin/JoinServerMixin.java
+++ src/main/java/io/github/hotlava03/chatutils/mixin/JoinServerMixin.java
@@ -1,15 +1,8 @@
 package io.github.hotlava03.chatutils.mixin;
 
 import io.github.hotlava03.chatutils.events.JoinServerEvent;
-import net.minecraft.client.MinecraftClientGame;
 import net.minecraft.client.gui.screen.ConnectScreen;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.packet.s2c.play.DisconnectS2CPacket;
-import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
--- src/main/java/io/github/hotlava03/chatutils/mixin/ReceiveMessageMixin.java
+++ src/main/java/io/github/hotlava03/chatutils/mixin/ReceiveMessageMixin.java
@@ -1,7 +1,6 @@
 package io.github.hotlava03.chatutils.mixin;
 
 import io.github.hotlava03.chatutils.events.MessageReceiveEvent;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.ChatHudLine;
 import net.minecraft.client.gui.hud.MessageIndicator;
@@ -10,7 +9,6 @@ import net.minecraft.text.*;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
