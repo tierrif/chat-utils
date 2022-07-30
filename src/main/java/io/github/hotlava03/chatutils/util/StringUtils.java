a15981b21e864e3e859f477298e35fafd4efe157
tierrif <tierriff@gmail.com>
2022-07-30 16:35:42 +0100

    Fix merge issue.

--- src/main/java/io/github/hotlava03/chatutils/util/StringUtils.java
+++ src/main/java/io/github/hotlava03/chatutils/util/StringUtils.java
@@ -1,52 +1,42 @@
-package io.github.hotlava03.chatutils.listeners;
+package io.github.hotlava03.chatutils.util;
 
 import io.github.hotlava03.chatutils.config.ChatUtilsConfig;
-import io.github.hotlava03.chatutils.events.EventHandler;
-import io.github.hotlava03.chatutils.events.types.MessageReceiveEvent;
-import io.github.hotlava03.chatutils.events.types.MessageReceiveListener;
-import net.minecraft.text.*;
+import net.kyori.adventure.platform.fabric.FabricClientAudiences;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.minecraft.text.Text;
 
-import java.util.regex.Pattern;
+import static org.apache.commons.lang3.StringUtils.getLevenshteinDistance;
 
-import static io.github.hotlava03.chatutils.util.StringUtils.textToLegacy;
-import static io.github.hotlava03.chatutils.util.StringUtils.translateAlternateColorCodes;
-
-public class CopyChatListener extends MessageReceiveListener {
-    // Taken from https://github.com/SpigotMC/BungeeCord
-    private static final Pattern STRIP_COLOR_PATTERN = Pattern.compile("(?i)§([0-9A-FK-OR]|#[a-f0-9]{6})");
-
-    @Override
-    public void onMessageReceive(MessageReceiveEvent e) {
-        String tooltip;
-        String toCopy = textToLegacy(e.getText());
-        if (!ChatUtilsConfig.COPY_COLORS.value()) {
-            toCopy = STRIP_COLOR_PATTERN.matcher(toCopy).replaceAll("");
-        } else {
-            toCopy = STRIP_COLOR_PATTERN.matcher(toCopy).replaceAll(matchResult ->
-                    matchResult.group().replace("§", "&"));
+public class StringUtils {
+    public static String translateAlternateColorCodes(String textToTranslate) {
+        char[] b = textToTranslate.toCharArray();
+        for (int i = 0; i < b.length - 1; ++i) {
+            if (b[i] == '&' && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i + 1]) > -1) {
+                b[i] = '§';
+                b[i + 1] = Character.toLowerCase(b[i + 1]);
+            }
         }
-
-        if (ChatUtilsConfig.PREVIEW_CONTENT.value()) {
-            tooltip = translateAlternateColorCodes(
-                    ChatUtilsConfig.COPY_TO_CLIPBOARD_MESSAGE.value() + "\n\n&9Preview:\n&f") + toCopy;
-        } else {
-            tooltip = translateAlternateColorCodes(ChatUtilsConfig.COPY_TO_CLIPBOARD_MESSAGE.value());
-        }
-
-        Style style = e.getText().getStyle()
-                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/chatmacros " + toCopy));
-        if (ChatUtilsConfig.TOOLTIP_ENABLED.value()) {
-            style = style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                    Text.literal(tooltip)));
-        }
-
-        if (e.getText().getStyle().getClickEvent() == null && ChatUtilsConfig.ENABLED.value()) {
-            e.getTextAsMutable().setStyle(style);
-        }
+        return new String(b);
     }
 
-    @Override
-    public EventHandler.EventType getType() {
-        return EventHandler.EventType.MESSAGE_RECEIVE;
+    /*
+     * This method does not belong to me.
+     * Original source:
+     * https://github.com/killjoy1221/TabbyChat-2/blob/master/src/main/java/mnm/mods/tabbychat/extra/ChatAddonAntiSpam.java
+     */
+    public static double getDifference(String s1, String s2) {
+        double avgLen = (s1.length() + s2.length()) / 2D;
+        if (avgLen == 0) {
+            return 0;
+        }
+        return getLevenshteinDistance(s1.toLowerCase(), s2.toLowerCase()) / avgLen;
+    }
+
+    public static String textToLegacy(Text text) {
+        var builder = LegacyComponentSerializer.builder()
+                .character(LegacyComponentSerializer.SECTION_CHAR)
+                .flattener(FabricClientAudiences.of().flattener());
+        if (ChatUtilsConfig.COPY_HEX_COLORS.value()) builder.hexColors();
+        return builder.build().serialize(text.asComponent());
     }
 }
