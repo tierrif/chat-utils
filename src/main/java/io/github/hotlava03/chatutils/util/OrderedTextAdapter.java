03f14543a68f3dc3ecc529b01cdf6f0520daf771
Tierri Ferreira <tierriff@gmail.com>
2023-07-13 14:47:12 +0100

    Remove unused code.

--- src/main/java/io/github/hotlava03/chatutils/util/OrderedTextAdapter.java
+++ src/main/java/io/github/hotlava03/chatutils/util/OrderedTextAdapter.java
@@ -2,9 +2,6 @@ package io.github.hotlava03.chatutils.util;
 
 import net.minecraft.text.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class OrderedTextAdapter {
     public static String orderedTextToString(OrderedText text) {
         var visitor = new TextVisitor();
@@ -12,32 +9,17 @@ public class OrderedTextAdapter {
         return visitor.toString();
     }
 
-    public static MutableText orderedTextToMutableText(OrderedText text) {
-        var visitor = new TextVisitor();
-        text.accept(visitor);
-        List<MutableText> list = visitor.getTextList();
-        return list.stream().reduce(MutableText::append).orElse(null);
-    }
-
     private static class TextVisitor implements CharacterVisitor {
         StringBuilder sb = new StringBuilder();
-        List<MutableText> textList = new ArrayList<>();
 
         @Override
         public String toString() {
             return sb.toString();
         }
 
-        public List<MutableText> getTextList() {
-            return textList;
-        }
-
         @Override
         public boolean accept(int index, Style style, int codePoint) {
             sb.appendCodePoint(codePoint);
-            MutableText text = MutableText.of(new LiteralTextContent(new String(Character.toChars(codePoint))));
-            text.setStyle(style);
-            textList.add(text);
             return true;
         }
     }
